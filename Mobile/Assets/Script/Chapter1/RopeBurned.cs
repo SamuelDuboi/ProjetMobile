74046f1ffd7f8b768dbf067ae2934a8262efc86e
Dubois <s.dubois@rubika-edu.com>
2021-03-15 14:27:41 +0100

    Push for Theo

---
+++ Mobile/Assets/Script/Chapter1/RopeBurned.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RopeBurned : MonoBehaviour
+{
+
+    public Animator burnedRope;
+    private bool doOnce;
+    private void Start()
+    {
+        EventManager.instance.LightObject += LightOn;
+    }
+    public void LightOn(GameObject _gameObject)
+    {
+        if (gameObject == _gameObject && !doOnce)
+        {
+            doOnce = true;
+            burnedRope.SetTrigger("Interact");
+        }
+    }
+}
