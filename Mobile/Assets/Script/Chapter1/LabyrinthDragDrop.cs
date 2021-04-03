ad7600f2c49e54c0c592d53645fe76d60aee02af
sam <s.dubois@rubika-edu.com>
2021-04-03 14:21:39 +0200

    Labyrinth

--- Mobile/Assets/Script/Chapter1/LabyrinthDragDrop.cs
+++ Mobile/Assets/Script/Chapter1/LabyrinthDragDrop.cs
@@ -9,26 +9,41 @@ public class LabyrinthDragDrop : MonoBehaviour, IDragHandler
 
     RectTransform[] wallRect;
     public GameObject[] walls;
+    public GameObject end ;
+    RectTransform endRect;
+
+    RectTransform thisRect;
+    public GameObject canvas;
     private void Start()
     {
+        thisRect = transform as RectTransform;
         initialPos = transform.position;
         wallRect = new RectTransform[walls.Length];
         for (int i = 0; i < walls.Length; i++)
         {
             wallRect[i] = walls[i].transform as RectTransform;
         }
+        endRect = end.transform as RectTransform;
     }
     public void OnDrag(PointerEventData eventData)
     {
         transform.position = Input.GetTouch(0).position;
         foreach (RectTransform rect in wallRect)
         {
-            if(RectTransformUtility.RectangleContainsScreenPoint(rect, Input.GetTouch(0).position))
+            if(RectTransformUtility.RectangleContainsScreenPoint(rect, thisRect.position))
             {
                 transform.position = initialPos;
                 eventData.pointerDrag = null;
                 return;
             }
+        }
+        if (RectTransformUtility.RectangleContainsScreenPoint(endRect, thisRect.position))
+        {
+            GetComponentInParent<ObjectHandler>().trialInstantiate = null;
+            GetComponentInParent<ObjectHandler>().interactifElement.spawnNewTrial = false;
+            GetComponentInParent<ObjectHandler>().Interact(GetComponentInParent<ObjectHandler>().HitBoxZoom.gameObject);
+            Destroy(canvas);
+            return;
         }
     }
 
