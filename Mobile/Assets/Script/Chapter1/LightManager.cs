b8d542f48a8190afadeb3149d7d6936a865dbcba
sam <s.dubois@rubika-edu.com>
2021-04-03 16:43:39 +0200

    fix salle 1 et 2

--- Mobile/Assets/Script/Chapter1/LightManager.cs
+++ Mobile/Assets/Script/Chapter1/LightManager.cs
@@ -22,6 +22,15 @@ public class LightManager : MonoBehaviour
         LunchCastLight(gameObject);
     }
 
+    private void OnDrawGizmos()
+    {
+        Handles.color = Color.red;
+        Handles.ArrowHandleCap(0, this.transform.position + this.transform.forward * 0.25f,this.transform.rotation, 0.5f, EventType.Repaint);
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position, 0.25f);
+        DrawPattern(this.transform.position + this.transform.forward * .75f, this.transform.forward, maxReflectionCount);
+    }
+
     private void Update()
     {
         if (!cantCast )
