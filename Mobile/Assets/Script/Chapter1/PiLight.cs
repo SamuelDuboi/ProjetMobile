29435bd8169d956350ddd99bf46061e1a9e5fae5
sam <s.dubois@rubika-edu.com>
2021-04-05 11:37:55 +0200

    Push For Marine

---
+++ Mobile/Assets/Script/Chapter1/PiLight.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PiLight : MonoBehaviour
+{
+    public MeshRenderer numberLighted;
+    private bool doOnce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        EventManager.instance.LightObject += LightOn;
+    }
+
+   private void LightOn (GameObject gameObject)
+    {
+        if(gameObject = this.gameObject)
+        {
+            if (!doOnce)
+            {
+                numberLighted.enabled = true;
+                doOnce = true;
+            }
+        }
+    }
+}
