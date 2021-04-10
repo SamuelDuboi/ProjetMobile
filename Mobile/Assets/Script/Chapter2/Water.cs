4215d13febfcdccafcbbc5c75db2ead6838c604a
sam <s.dubois@rubika-edu.com>
2021-04-10 11:31:01 +0200

    Water + improve anims

--- Mobile/Assets/Script/Chapter2/Water.cs
+++ Mobile/Assets/Script/Chapter2/Water.cs
@@ -6,14 +6,29 @@ public class Water : MonoBehaviour
 {
 
     public Animator waterAnim;
+    public bool blockUp;
+    public bool blockDown;
     // Start is called before the first frame update
     void Start()
     {
         EventManager.instance.SwipeUp += UpsideDown;
+        waterAnim.SetBool("BlockDown", blockDown);
+        waterAnim.SetBool("BlockUp", blockUp);
     }
 
-   public virtual void UpsideDown(bool up)
+    public virtual void UpsideDown(bool up)
     {
         waterAnim.SetTrigger("UpsideDown");
+    }
+
+    public virtual void BlockUp()
+    {
+        blockUp = !blockUp;
+        waterAnim.SetBool("BlockUp", blockUp); 
+    }
+    public virtual void BlockDown()
+    {
+        blockDown = !blockDown;
+        waterAnim.SetBool("BlockDown", blockDown);
     }
 }
