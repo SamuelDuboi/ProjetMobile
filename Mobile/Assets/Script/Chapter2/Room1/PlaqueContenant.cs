7dfed95019998373323110f117dd078d28ebb392
sam <s.dubois@rubika-edu.com>
2021-04-12 21:18:13 +0200

    Room 1 done, Théo need some fixes but its done

---
+++ Mobile/Assets/Script/Chapter2/Room1/PlaqueContenant.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaqueContenant : ObjectHandler
+{
+    public Water water;
+
+    public override void Interact(GameObject currentGameObject)
+    {
+        if (HitBoxZoom != null && currentGameObject == HitBoxZoom.gameObject)
+        {
+           
+            if (interactifElement.hasLinkGameObject)
+            {
+                int numberOfObject = 0;
+                foreach (var curentGamObject in interactifElement.ObjectoOpen)
+                {
+                    foreach (var inventoryItem in InventoryManager.Instance.interactifElementsList)
+                    {
+                        if (inventoryItem.name == curentGamObject)
+                            numberOfObject++;
+                    }
+                }
+                if (numberOfObject != interactifElement.ObjectoOpen.Count)
+                {
+                    interactifElement.interactionAnimator.SetLayerWeight(1, 1);
+                    interactifElement.interactionAnimator.SetLayerWeight(2, 0);
+                    interactifElement.interactionAnimator.SetTrigger("Interact");
+                    return;
+                }
+                interactifElement.interactionAnimator.SetLayerWeight(1, 0);
+                interactifElement.interactionAnimator.SetLayerWeight(2, 1);
+                water.BlockUp();
+            }
+           
+        }
+    }
+}
